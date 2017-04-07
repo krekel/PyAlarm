585ac675b8b78d3a94b25cf940b94908d6a124d4
krekel <luisg.diaz@gmail.com>
2017-04-06 20:45:22 -0400

    Fixed invalid entries on hour and minutes.

--- PyAlarm.py
+++ PyAlarm.py
@@ -1,42 +1,44 @@
-#! /usr/bin/env python3.5
+#! /usr/bin/env python3
 import argparse
-import re
+import sys
 import time
 import webbrowser
 import random
 
 
 def main():
+    try:
+        parser = argparse.ArgumentParser(description='Video alarm')
+        parser.add_argument('mode', choices=['alarm', 'random'],
+                            help='Choose a mode. Alarm mode works only with 24hr format')
+        parser.add_argument('hour', type=validate, nargs='?', default='00')
+        parser.add_argument('minutes', type=validate, nargs='?', default='00')
+        parser.add_argument('--span', type=int, nargs='?', const=2, help='Random Mode: Set time span in hours')
+        args = parser.parse_args()
+        print(args)
 
-    parser = argparse.ArgumentParser(description='Video alarm')
-    parser.add_argument('mode', choices=['alarm', 'random'],
-                        help='Choose a mode. Alarm mode works only with 24hr format')
-    parser.add_argument('hour', type=validate, nargs='?', default='00')
-    parser.add_argument('minutes', type=validate, nargs='?', default='00')
-    parser.add_argument('--span', type=int, nargs='?', const=2, help='Random Mode: Set time span in hours')
-    args = parser.parse_args()
-    print(args)
+        if args.mode == 'random':
+            if args.span is not None:
+                span = int(time.strftime('%H')) + args.span
+            else:
+                span = int(time.strftime('%H')) + 2
 
-    if args.mode == 'random':
-        if args.span is not None:
-            span = int(time.strftime('%H')) + args.span
+            h = random.randint(int(time.strftime('%H')), int(span))
+            r_minute = '{:02d}'.format(random.randint(int(time.strftime('%M')), 59))
+
+            if h >= 24:
+                r_hour = '{:02d}'.format(h - 24)
+            else:
+                r_hour = '{:02d}'.format(h)
+
+            r_time = r_hour + r_minute
+            print(r_time)
+            open_browser(r_time)
         else:
-            span = int(time.strftime('%H')) + 2
-
-        h = random.randint(int(time.strftime('%H')), int(span))
-        r_minute = '{:02d}'.format(random.randint(int(time.strftime('%M')), 59))
-
-        if h >= 24:
-            r_hour = '{:02d}'.format(h - 24)
-        else:
-            r_hour = '{:02d}'.format(h)
-
-        r_time = r_hour + r_minute
-        print(r_time)
-        open_browser(r_time)
-    else:
-        alarm = args.hour + args.minutes
-        open_browser(alarm)
+            alarm = args.hour + args.minutes
+            open_browser(alarm)
+    except KeyboardInterrupt:
+        print()
 
 
 def open_browser(input_time):
@@ -53,10 +55,12 @@ def open_browser(input_time):
 
 
 def validate(value):
-    time.strptime(value, '%H')
-    time.strptime(value, '%M')
-    return value
+    if 0 <= int(value) <= 23 or 0 <= int(value) <= 59:
+        return '{:02d}'.format(int(value))
+    else:
+        raise argparse.ArgumentError
 
 
 if __name__ == '__main__':
     main()
+    sys.exit(0)
